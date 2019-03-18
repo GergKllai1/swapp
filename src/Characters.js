c404eeaf59cf5f797b6115b7d6778b61815bb28e
Greg <gerg.kallai@gmail.com>
2019-03-18 15:48:21 +0100

    Fetches first name from API
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- src/Characters.js
+++ src/Characters.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import axios from 'axios'
+
+const URL = 'https://swapi.co/api/'
 
 export class Characters extends Component {
     state = {
-        text: 'World'
+        name: ''
+    }
+
+    async componentDidMount() {
+        const response = await axios.get('https://swapi.co/api/people/1')
+        const name = response.data.name;
+        this.setState({
+            name: name
+        })
     }
   render() {
     return (
       <View>
-        <Text style={this.props.style}> Hello {this.state.text}  </Text>
+        <Text style={this.props.style}> Hello {this.state.name}  </Text>
       </View>
     )
   }
