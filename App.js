8e34ac7f43651c7dfafa9f8200c51de58f350075
Greg <gerg.kallai@gmail.com>
2019-03-20 12:14:21 +0100

    Adds navigation to app.js
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Layout from './src/Layout';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import Characters from './src/Characters'
+import CharacterDetails from './src/CharacterDetails'
 
 
-const App = () => {
-    return <Layout />
-  }
 
-export default App;
+// const App = () => {
+//     return <Layout />
+//   }
+
+const App = createStackNavigator({
+  Layout: {
+    screen: Layout,
+  },
+  CharacterDetails: {
+    screen: CharacterDetails,
+  },
+}, {
+    initialRouteName: 'Layout',
+});
+
+
+export default createAppContainer(App);
